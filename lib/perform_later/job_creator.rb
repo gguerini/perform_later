497ae4c9716b0f1ce72d04ea40756454564a53aa
David Larrabee <david.larrabee@meyouhealth.com>
2014-02-06 10:37:23 -0500

    Send to the scheduler in expected format; cleanup
      * queue, timestamp, klass, args
      * no need to check args.length, splat does what we need

--- lib/perform_later/job_creator.rb
+++ lib/perform_later/job_creator.rb
@@ -1,6 +1,6 @@
 module PerformLater
   class JobCreator
-    
+
     attr_reader :queue, :worker, :klass_name, :id, :method
     attr_accessor :args
 
@@ -14,8 +14,12 @@ module PerformLater
     end
 
     def enqueue(delay=nil)
-      return Resque.enqueue_in_with_queue(queue, delay, worker, klass_name, id, method, args) if delay 
+      return Resque.enqueue_in_with_queue(queue, delay, worker, *delay_args) if delay
       Resque::Job.create(queue, worker, klass_name, id, method, *args)
+    end
+
+    def delay_args
+      [ klass_name, id, method ] + Array(args)
     end
   end
 end
