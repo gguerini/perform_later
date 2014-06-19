ef85a1e22da7a91664cd75517b1a6274b7c11f90
Guillermo Guerini <guillermo@gguerini.com>
2014-06-19 11:07:22 -0400

    Add environment validation.

--- lib/resque_mailer_patch.rb
+++ lib/resque_mailer_patch.rb
@@ -2,13 +2,21 @@ module Resque
   module Mailer
     module ClassMethods
       def perform(action, *args)
+        return if environment_excluded?
+
         args = PerformLater::ArgsParser.args_from_resque(args)
         self.send(:new, action, *args).message.deliver
+      end
+
+      def environment_excluded?
+        !ActionMailer::Base.perform_deliveries || excluded_environment?(current_env)
       end
     end
 
     class MessageDecoy
       def deliver
+        return deliver! if environment_excluded?
+
         args = PerformLater::ArgsParser.args_to_resque(@args)
         resque.enqueue(@mailer_class, @method_name, *args)
       end
